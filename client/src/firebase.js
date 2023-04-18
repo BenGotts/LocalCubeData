9e383026fccba3cf3f84cd1f1bb327bfb0489d37
BenGotts <837gb1@gmail.com>
2023-04-18 01:16:56 -0700

    Add Firebase

---
+++ client/src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAWOS82XEel3F3oY36BHDMwOhOren62PR8",
+    authDomain: "localcubedata.firebaseapp.com",
+    projectId: "localcubedata",
+    storageBucket: "localcubedata.appspot.com",
+    messagingSenderId: "869499316871",
+    appId: "1:869499316871:web:faa2a74b8cabd6f0bd6622",
+    measurementId: "G-CPYFVDHFK6"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Initialize Firestore
+const db = firebase.firestore();
+
+export default db;
